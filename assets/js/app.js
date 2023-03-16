409c1eb62edcdd7717f0cd1a97e186e44938a0b4
unknown <ngocthien250800@gmail.com>
2023-03-16 22:28:54 +0700

    update1

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ $(".customer__list").slick({
   infinite: true,
   dots: true,
   autoplay: true,
-  autoplaySpeed: 2000,
+  autoplaySpeed: 4000,
   prevArrow:
     "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-arrow-left' aria-hidden='true'></i></button>",
   nextArrow:
